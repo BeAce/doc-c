8c914a426e560bc4ecf4a808a6d98d83a60f4811
BeAce <beaceshimin@gmail.com>
2017-05-15 10:13:52 +0800

    new stage

---
+++ stage6/input.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	int flag = 1;
+	int i;
+	int count = 0;
+	int sum = 0;
+	while(flag)
+	{
+		scanf("%d", &i);
+		if(0 == i)
+		{
+			break;
+		}
+		count ++;
+		sum+=i;
+	}
+	printf("%d, %d\n", sum, count);
+	return 0;
+}
